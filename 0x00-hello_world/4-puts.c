945e6079b5041685a28df86141933e7691e35a6f
amen godwin <godwininfo98@gmail.com>
2022-06-13 12:16:12 -0700

    initial commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,11 @@
 
 /**
  * main - print a string to the screen
+ *
+ * Return: (0) success
  */
 int main(void)
 {
- 	puts("\"programming is like building  multilingual puzzle");
+	puts("\"programming is like building a multilingual puzzle");
 	return (0);
-}
+}	
