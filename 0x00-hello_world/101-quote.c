879a367e3dd9bab145be7afd8f918cf02b92ffcc
amen godwin <godwininfo98@gmail.com>
2022-06-14 03:56:27 -0700

    initial commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * 			followed by a new line,to standard error.
+ * Return (1) success
+ */
+
+int main(void)
+{
+	write(2,
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+		61);
+
+	return (1);
+}
