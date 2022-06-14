e4d4efcd5692b54bff5ed02e55f2e36e924f866f
amen godwin <godwininfo98@gmail.com>
2022-06-14 14:07:11 -0700

    initial commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
-
+#include <unistd.h>
 /**
- * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- * 			followed by a new line,to standard error.
- * Return (1) success
+ * main - entry point
+ *
+ * the string in the write variable is printed
+ * Return: Always 1
  */
 
 int main(void)
 {
-	write(2,
-		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-		61);
-
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
