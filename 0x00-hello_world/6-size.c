ba0f8b2f77a6a4874ff50a725bc9a3f5b65bb873
amen godwin <godwininfo98@gmail.com>
2022-06-13 23:52:52 -0700

    initial commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,8 +9,8 @@
 int main(void)
 {
 	int a; long int b; long long int c;
-       	char d;
-       	float f;
+	char d;
+	float f;
 
 
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
