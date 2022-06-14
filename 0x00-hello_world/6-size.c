575ae5aff25f381b69c0e93333569e2bd6de8dae
amen godwin <godwininfo98@gmail.com>
2022-06-14 03:12:58 -0700

    initial commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,17 +2,17 @@
 
 /**
  * main - print a string to screen
- *
  * Return: (0) success
  */
 
 int main(void)
 {
-	int a; long
-	int b; long long 
+	int a;long
+	int b;long long 
 	int c;
 	char d;
 	float f;
+
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(d));
 
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(a));
@@ -22,5 +22,6 @@ int main(void)
 	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+
 	return (0);
 }
