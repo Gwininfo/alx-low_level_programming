9641a40a6018ab551a830e23361a1f0f53be8bf3
amen godwin <godwininfo98@gmail.com>
2022-06-16 15:53:52 -0700

    initial commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print letters to screen
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		if (letter != 'e' && letter != 'q')
+			putchat (letter);
+	}
+	putchar("\n");
+	return (0);
+}
