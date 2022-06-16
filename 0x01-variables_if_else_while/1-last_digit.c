bad8dc885240ec3c10f2f87623e060dbee307fb2
amen godwin <godwininfo98@gmail.com>
2022-06-16 14:38:10 -0700

    initial commit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,13 +10,13 @@
 int main(void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
 	if ((n % 10) > 5)
 	{
-		printf("last digit of %d is %d and is greater than 5
-				\n", n, n % 10);
+		printf("Last digit of %d is %d and is greater than 5\n", n, n % 10);
 	}
 	else if ((n % 10) < 6 && (n % 10) != 0)
 	{
@@ -26,5 +26,6 @@ int main(void)
 	{
 		printf("last digit of %d is %d and is 0\n", n, n % 10);
 	}
+
 	return (0);
 }
